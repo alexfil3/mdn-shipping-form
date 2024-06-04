b72cc8c02b36e09495dbaf806532d951ad6848f3
Alex Filimonov <alex.filimonov2002@gmail.com>
2024-06-04 16:12:16 +0300

    completes the form

---
+++ index.js
@@ -0,0 +1,26 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    document
+      .getElementById("billing-checkbox")
+      .addEventListener("change", toggleBilling);
+  },
+  false
+);
+
+function toggleBilling() {
+  const billingItems = document.querySelectorAll('#billing input[type="text"]');
+  const billingLabels = document.querySelectorAll(".billing-label");
+
+  for (let i = 0; i < billingItems.length; i++) {
+    billingItems[i].disabled = !billingItems[i].disabled;
+
+    if (
+      billingLabels[i].getAttribute("class") === "billing-label disabled-label"
+    ) {
+      billingLabels[i].setAttribute("class", "billing-label");
+    } else {
+      billingLabels[i].setAttribute("class", "billing-label disabled-label");
+    }
+  }
+}
